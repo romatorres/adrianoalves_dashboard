ee152e39cae9aa021e728d50efe10375d5963e6f
romatorres <romatorres12@gmail.com>
2025-02-05 06:45:51 -0300

    logo/login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,24 @@
 "use client";
 
 import { LoginForm } from "@/components/Auth/LoginForm";
+import Image from "next/image";
 
 export default function Login() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-secondary">
       <div className="max-w-xl w-full p-8 bg-white shadow-md rounded-lg">
-        <h1 className="text-2xl font-bold text-center mb-10">Login</h1>
+        <div className="flex flex-col justify-center items-center gap-10">
+          <div>
+            <Image
+              src="/img/logo.png"
+              alt="Logo Barbearia"
+              width={200}
+              height={50}
+              className="object-contain"
+            />
+          </div>
+          <h1 className="text-2xl font-bold text-center mb-10">Login</h1>
+        </div>
         <LoginForm />
       </div>
     </div>
